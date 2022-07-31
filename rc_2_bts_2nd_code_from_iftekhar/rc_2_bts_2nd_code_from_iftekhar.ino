15d039c76d8b311f77c7d83434fc83fe50ab2b0b
Mahmudul <work.mahmudulhasan@gmail.com>
2022-07-31 17:48:07 +0600

    rc 2 bts edited

--- rc_2_bts_2nd_code_from_iftekhar/rc_2_bts_2nd_code_from_iftekhar.ino
+++ rc_2_bts_2nd_code_from_iftekhar/rc_2_bts_2nd_code_from_iftekhar.ino
@@ -1,4 +1,3 @@
-#include <SoftwareSerial.h>
 int L_EN_FOR_ONE=3;
 int R_EN_FOR_ONE=4;
 int L_PWM_FOR_ONE=5;
@@ -8,10 +7,12 @@ int R_EN_FOR_TWO=9;
 int L_PWM_FOR_TWO=10;
 int R_PWM_FOR_TWO=11;
 char incomingByte; // for incoming serial data
-int speed_min = 50; //the minimum "speed" the motors will turn - take it lower and motors don't turn
+int speed_min = 155; //the minimum "speed" the motors will turn - take it lower and motors don't turn
 int speed_max = 255; //
+
 int speed_left = speed_max; // set both motors to maximum speed
 int speed_right = speed_max;
+
 void left();
 void right();
 void forward();
@@ -30,6 +31,7 @@ pinMode(L_EN_FOR_TWO,OUTPUT);
 pinMode(R_EN_FOR_TWO,OUTPUT);
 pinMode(L_PWM_FOR_TWO,OUTPUT);
 pinMode(R_PWM_FOR_TWO,OUTPUT);
+
 digitalWrite(L_EN_FOR_ONE,HIGH);
 digitalWrite(R_EN_FOR_ONE,HIGH);
 digitalWrite(L_EN_FOR_TWO,HIGH);
@@ -46,135 +48,135 @@ if (Serial.available() > 0) {
      case 'S':
       {
         stopo();
-       //Serial.println("Stop\n");
+       //Serial.println("Stop\n"); 
        incomingByte='*';}
      break;
      
-     case 'L':
-     
+     case 'F':
+      
      {  left();
        
       // Serial.println("Forward\n");
        incomingByte='*';}
      break;
-   
-      case 'R':
+    
+      case 'B':
        
     {   right();
       // Serial.println("Backward\n");
        incomingByte='*';}
      break;
      
-     case 'F':
+     case 'L':
      // turn right
      {  
-       forward();
+       forward(); 
       // Serial.println("Rotate Right\n");
        incomingByte='*';}
      break;
-       case 'B':
-      {
-       backward();    
+       case 'R':
+      { 
+       backward();     
        //Serial.println("Rotate Left\n");
        incomingByte='*';}
      break;
      case '1':
-       
-      { speed_left = 20;
+        
+      { speed_left = 20; 
        speed_right = 20;
        //Serial.println("Speed 1\n");
        incomingByte='*';}
      break;
     case '2':
-      {
-        speed_left = 40;
+      { 
+        speed_left = 40; 
        speed_right = 40;
        //Serial.println("Speed 2 \n");
        incomingByte='*';}
      break;
     case '3':
-      {
-        speed_left = 60;
+      { 
+        speed_left = 60; 
        speed_right = 60;
        //Serial.println("Speed 3 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '4':
-      {
-        speed_left = 80;
+      { 
+        speed_left = 80; 
        speed_right = 80;
        //Serial.println("Speed 4 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '5':
-      {
-        speed_left = 100;
+      { 
+        speed_left = 100; 
        speed_right = 100;
        //Serial.println("Speed 5 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '6':
-      {
-        speed_left = 120;
+      { 
+        speed_left = 120; 
        speed_right = 120;
        //Serial.println("Speed 6 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '7':
-      {
-        speed_left = 140;
+      { 
+        speed_left = 140; 
        speed_right = 140;
       // Serial.println("Speed 7 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '8':
-      {
-        speed_left = 160;
+      { 
+        speed_left = 160; 
        speed_right = 160;
        //Serial.println("Speed 8 \n");
        incomingByte='*';}
-     break;
+     break; 
         case '9':
-      {
-        speed_left = 200;
+      { 
+        speed_left = 200; 
        speed_right = 200;
        //Serial.println("Speed 9 \n");
        incomingByte='*';}
-     break;
+     break; 
         case 'q':
-      {
-        speed_left = 255;
+      { 
+        speed_left = 255; 
        speed_right = 255;
        Serial.println("Speed full \n");
        incomingByte='*';}
-     break;
-        case 'J':
-      {
+     break; 
+        case 'G':
+      { 
        back_right();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'H':
-      {
+      case 'J':
+      { 
        back_left();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'I':
-      {
+      case 'H':
+      { 
        forward_right();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;
-      case 'G':
-      {
+      case 'I':
+      { 
       forward_left();
        Serial.println("Speed full \n");
        incomingByte='*';}
      break;      
   }
 
- 
+  
 }
 void forward(){
 
@@ -200,8 +202,8 @@ void left(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,speed_right);
   analogWrite(L_PWM_FOR_TWO,0);
- 
- 
+  
+  
   };
 void stopo(){
 
@@ -209,16 +211,16 @@ void stopo(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
- 
- 
+  
+  
   };
 void forward_left(){
    analogWrite(R_PWM_FOR_ONE,0);
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,speed_right);
   analogWrite(L_PWM_FOR_TWO,0);
- 
- 
+  
+  
   };
 void forward_right(){
      analogWrite(R_PWM_FOR_ONE,0);
@@ -231,13 +233,13 @@ void back_left(){
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,speed_right);
- 
+  
   };
 void back_right(){
       analogWrite(R_PWM_FOR_ONE,speed_left);
   analogWrite(L_PWM_FOR_ONE,0);
   analogWrite(R_PWM_FOR_TWO,0);
   analogWrite(L_PWM_FOR_TWO,0);
- 
- 
+  
+  
   };
