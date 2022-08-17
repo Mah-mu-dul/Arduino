a49d011b140e491fceae1c7ccdfa05c2bcb32401
mahmudul <work.mahmudulhasan@gmail.com>
2022-08-17 18:30:26 +0600

    2L2n8 done

--- 2_L298N_RC/2_L298N_RC.ino
+++ 2_L298N_RC/2_L298N_RC.ino
@@ -1,12 +1,20 @@
-int ENA = 5;
-int INA = 6;
-int INB = 7;
+int ENA_L =2;
+int IN1_L =3;
+int IN2_L =4; 
+int IN3_L =5; 
+int IN4_L =6;
+int ENB_L =7;
 
-int INC = 8;
-int IND = 9;
-int ENB = 10;
 
-char incomingByte; // for incoming serial data
+//RIGHT
+int ENA_R= 8;
+int IN1_R =9;
+int IN2_R =10; 
+int IN3_R =11; 
+int IN4_R =12;
+int ENB_R =13;
+
+char incomingByte=0; // for incoming serial data
 int speed_min = 135; //the minimum "speed" the motors will turn - take it lower and motors don't turn
 int speed_max = 255; //
 
@@ -22,17 +30,26 @@ void forward_right();
 void back_left();
 void back_right();
 void setup() {
-  Serial.begin(9600);
-pinMode(ENA,OUTPUT);
-pinMode(INA,OUTPUT);
-pinMode(INB,OUTPUT);
-pinMode(INC,OUTPUT);
-pinMode(IND,OUTPUT);
-pinMode(ENB,OUTPUT);
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+pinMode(ENA_L,OUTPUT);
+pinMode(IN1_L,OUTPUT);
+pinMode(IN2_L,OUTPUT);
+pinMode(IN3_L,OUTPUT);
+pinMode(IN4_L,OUTPUT);
+pinMode(ENB_L,OUTPUT);
+  // put your setup code here, to run once:
+pinMode(ENA_R,OUTPUT);
+pinMode(IN1_R,OUTPUT);
+pinMode(IN2_R,OUTPUT);
+pinMode(IN3_R,OUTPUT);
+pinMode(IN4_R,OUTPUT);
+pinMode(ENB_R,OUTPUT);
+
 }
 
 void loop() {
-
+  // put your main code here, to run repeatedly:
 if (Serial.available() > 0) {
     incomingByte = Serial.read();
     }
@@ -40,99 +57,99 @@ if (Serial.available() > 0) {
   {
      case 'S':
       {
-        stopo();
+        stopy();
        //Serial.println("Stop\n"); 
        incomingByte='*';}
      break;
      
-     case 'F':
+     case 'L':
       
-     {  forward();
+     {  left();
        
       // Serial.println("Forward\n");
        incomingByte='*';}
      break;
     
-      case 'B':
+      case 'R':
        
-    {   backward();
+    {   right();
       // Serial.println("Backward\n");
        incomingByte='*';}
      break;
      
-     case 'L':
+     case 'F':
      // turn right
      {  
-       right(); 
+       forward(); 
       // Serial.println("Rotate Right\n");
        incomingByte='*';}
      break;
-       case 'R':
+       case 'B':
       { 
-       left();     
+       backward();     
        //Serial.println("Rotate Left\n");
        incomingByte='*';}
      break;
      case '1':
         
-      { speed_left = 100; 
-       speed_right = 100;
+      { speed_left = 20; 
+       speed_right = 20;
        //Serial.println("Speed 1\n");
        incomingByte='*';}
      break;
     case '2':
       { 
-        speed_left = 130; 
-       speed_right = 130;
+        speed_left = 40; 
+       speed_right = 40;
        //Serial.println("Speed 2 \n");
        incomingByte='*';}
      break;
     case '3':
       { 
-        speed_left = 140; 
-       speed_right = 140;
+        speed_left = 60; 
+       speed_right = 60;
        //Serial.println("Speed 3 \n");
        incomingByte='*';}
      break; 
         case '4':
       { 
-        speed_left = 150; 
-       speed_right = 150;
+        speed_left = 80; 
+       speed_right = 80;
        //Serial.println("Speed 4 \n");
        incomingByte='*';}
      break; 
         case '5':
       { 
-        speed_left = 170; 
-       speed_right = 170;
+        speed_left = 100; 
+       speed_right = 100;
        //Serial.println("Speed 5 \n");
        incomingByte='*';}
      break; 
         case '6':
       { 
-        speed_left = 180; 
-       speed_right = 180;
+        speed_left = 120; 
+       speed_right = 120;
        //Serial.println("Speed 6 \n");
        incomingByte='*';}
      break; 
         case '7':
       { 
-        speed_left = 200; 
-       speed_right = 200;
+        speed_left = 140; 
+       speed_right = 140;
       // Serial.println("Speed 7 \n");
        incomingByte='*';}
      break; 
         case '8':
       { 
-        speed_left = 210; 
-       speed_right = 210;
+        speed_left = 160; 
+       speed_right = 160;
        //Serial.println("Speed 8 \n");
        incomingByte='*';}
      break; 
         case '9':
       { 
-        speed_left = 220; 
-       speed_right = 220;
+        speed_left = 200; 
+       speed_right = 200;
        //Serial.println("Speed 9 \n");
        incomingByte='*';}
      break; 
@@ -143,108 +160,197 @@ if (Serial.available() > 0) {
        Serial.println("Speed full \n");
        incomingByte='*';}
      break; 
-//        case 'G':
-//      { 
-//       forward_left();
-//       Serial.println("Speed full \n");
-//       incomingByte='*';}
-//     break;
-//      case 'I':
-//      { 
-//       forward_right();
-//       Serial.println("Speed full \n");
-//       incomingByte='*';}
-//     break;
-//      case 'H':
-//      { 
-//       back_left();
-//       Serial.println("Speed full \n");
-//       incomingByte='*';}
-//     break;
-//      case 'J':
-//      { 
-//       back_right();
-//       Serial.println("Speed full \n");
-//       incomingByte='*';}
-//     break;      
+        case 'J':
+      { 
+       back_right();
+       Serial.println("Speed full \n");
+       incomingByte='*';}
+     break;
+      case 'H':
+      { 
+       back_left();
+       Serial.println("Speed full \n");
+       incomingByte='*';}
+     break;
+      case 'I':
+      { 
+       forward_right();
+       Serial.println("Speed full \n");
+       incomingByte='*';}
+     break;
+      case 'G':
+      { 
+      forward_left();
+       Serial.println("Speed full \n");
+       incomingByte='*';}
+     break;      
   }
-  
-  
-}
-void right(){
-analogWrite(ENA,speed_left);
-digitalWrite(INA,HIGH);
-digitalWrite(INB,LOW);
-digitalWrite(INC,HIGH);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
 
-  };
+}
 void left(){
-   analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,HIGH);
-digitalWrite(INC,LOW);
-digitalWrite(IND,HIGH);
-analogWrite(ENB,speed_right); 
-  };
-void backward(){
-analogWrite(ENA,speed_left);
-digitalWrite(INA,HIGH);
-digitalWrite(INB,LOW);
-digitalWrite(INC,LOW);
-digitalWrite(IND,HIGH);
-analogWrite(ENB,speed_right); 
-  };
-void forward(){
-  analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,HIGH);
-digitalWrite(INC,HIGH);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
+  //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,HIGH);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,HIGH);
+  analogWrite(ENB_L,speed_left);
+  //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,HIGH);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,HIGH);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,speed_right);
+
   
   };
-void stopo(){
-analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,LOW);
-digitalWrite(INC,LOW);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
+void right(){
+   //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,HIGH);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,HIGH);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,speed_left);
+  //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,HIGH);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,HIGH);
+  analogWrite(ENB_R,speed_right);
   
   };
 void forward_left(){
- analogWrite(ENA,speed_left);
-digitalWrite(INA,HIGH);
-digitalWrite(INB,LOW);
-digitalWrite(INC,LOW);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
+  //left
+  analogWrite(ENA_L,0);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,0);
+  
+  //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,HIGH);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,HIGH);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,speed_right);
   
   };
 void forward_right(){
-     analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,LOW);
-digitalWrite(INC,HIGH);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
+   //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,HIGH);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,HIGH);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,speed_left);
+
+ //right
+  analogWrite(ENA_R,0);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,0);
+ 
   };
 void back_left(){
- analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,LOW);
-digitalWrite(INC,LOW);
-digitalWrite(IND,HIGH);
-analogWrite(ENB,speed_right); 
+   //left
+  analogWrite(ENA_L,0);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,0);
+   //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,HIGH);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,HIGH);
+  analogWrite(ENB_R,speed_right);
+
   
   };
 void back_right(){
- analogWrite(ENA,speed_left);
-digitalWrite(INA,LOW);
-digitalWrite(INB,LOW);
-digitalWrite(INC,HIGH);
-digitalWrite(IND,LOW);
-analogWrite(ENB,speed_right); 
+  
+  //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,HIGH);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,HIGH);
+  analogWrite(ENB_L,speed_left);
+
+ //right
+  analogWrite(ENA_R,0);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,0);
+  
+  
+  };
+void forward(){
+ //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,HIGH);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,HIGH);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,speed_left);
+
+ //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,HIGH);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,HIGH);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,speed_right);
+
+
+  
+  };
+void backward(){
+ //left
+  analogWrite(ENA_L,speed_left);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,HIGH);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,HIGH);
+  analogWrite(ENB_L,speed_left);
+
+ //right
+  analogWrite(ENA_R,speed_right);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,HIGH);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,HIGH);
+  analogWrite(ENB_R,speed_right);
+
+
+  };
+
+void stopy(){
+  //
+    analogWrite(ENA_L,0);
+  digitalWrite(IN1_L,LOW);
+  digitalWrite(IN2_L,LOW);
+  digitalWrite(IN3_L,LOW);
+  digitalWrite(IN4_L,LOW);
+  analogWrite(ENB_L,0);
+  
+  analogWrite(ENA_R,0);
+  digitalWrite(IN1_R,LOW);
+  digitalWrite(IN2_R,LOW);
+  digitalWrite(IN3_R,LOW);
+  digitalWrite(IN4_R,LOW);
+  analogWrite(ENB_R,0);
+
+  
   };
